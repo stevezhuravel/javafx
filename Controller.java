4aec9974f228c560bb09799f8a0533e9954a6cd8
stevezhuravel <33463234+stevezhuravel@users.noreply.github.com>
2019-08-30 15:59:22 -0400

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,18 @@
+package javafx.example;
+
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class Controller {
+    
+    @FXML
+	private void openDialog() {
+		final FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showOpenDialog(new Stage());
+	}
+
+
+}
